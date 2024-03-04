7e05a21fd59d4c1e8e5ef61cc6de54d1de2fa983
Shon Benayoun <shonba98@gmail.com>
2024-03-04 19:24:14 +0200

    finish layout

--- src/Components/HomeArea/Home/Home.tsx
+++ src/Components/HomeArea/Home/Home.tsx
@@ -4,7 +4,7 @@ function Home(): JSX.Element {
     return (
         <div className="Home">
             <div className="homeParaContainer">
-                blab la 
+                blab la
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, ipsum asperiores illo magnam, accusantium facere repellat eum, iure praesentium aspernatur excepturi. Atque labore laboriosam, laudantium repudiandae at assumenda libero perferendis.em20</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, ipsum asperiores illo magnam, accusantium facere repellat eum, iure praesentium aspernatur excepturi. Atque labore laboriosam, laudantium repudiandae at assumenda libero perferendis.em20</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, ipsum asperiores illo magnam, accusantium facere repellat eum, iure praesentium aspernatur excepturi. Atque labore laboriosam, laudantium repudiandae at assumenda libero perferendis.em20</p>
@@ -18,9 +18,7 @@ function Home(): JSX.Element {
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, ipsum asperiores illo magnam, accusantium facere repellat eum, iure praesentium aspernatur excepturi. Atque labore laboriosam, laudantium repudiandae at assumenda libero perferendis.em20</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, ipsum asperiores illo magnam, accusantium facere repellat eum, iure praesentium aspernatur excepturi. Atque labore laboriosam, laudantium repudiandae at assumenda libero perferendis.em20</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, ipsum asperiores illo magnam, accusantium facere repellat eum, iure praesentium aspernatur excepturi. Atque labore laboriosam, laudantium repudiandae at assumenda libero perferendis.em20</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, ipsum asperiores illo magnam, accusantium facere repellat eum, iure praesentium aspernatur excepturi. Atque labore laboriosam, laudantium repudiandae at assumenda libero perferendis.em20</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, ipsum asperiores illo magnam, accusantium facere repellat eum, iure praesentium aspernatur excepturi. Atque labore laboriosam, laudantium repudiandae at assumenda libero perferendis.em20</p>
-            </div>
+         </div>
         </div>
     );
 }
