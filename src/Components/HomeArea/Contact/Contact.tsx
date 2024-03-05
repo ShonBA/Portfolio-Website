de0f8d937a8c3f88becda19b929c7bde6c93c83d
Shon Benayoun <shonba98@gmail.com>
2024-03-06 00:02:59 +0200

    add form logic

--- src/Components/HomeArea/Contact/Contact.tsx
+++ src/Components/HomeArea/Contact/Contact.tsx
@@ -1,27 +1,47 @@
+import { useForm } from "@formspree/react";
 import "./Contact.scss";
 
 function Contact(): JSX.Element {
+
+    const [state, handleSubmit] = useForm("mleqlear");
+
     return (
         <div className="Contact">
             <h1 className="headerLine">Contact</h1>
             <p>Feel free to Contact me by submitting the form below and I will get back to you as soon as possible.</p>
-            <form>
-                <div className="inputContainer">
-                    <label>Full Name</label>
-                    <input type="text" placeholder="Enter Your Name" />
-                </div>
-                <div className="inputContainer">
-                    <label>Email</label>
-                    <input type="email" placeholder="Enter Your Email" />
-                </div>
-                <div className="inputContainer">
-                    <label>Message</label>
-                    <textarea placeholder="Enter Your Message" />
-                </div>
-                <div className="formBtn">
-                    <button className="btn-p">Send</button>
-                </div>
-            </form>
+            {!state.succeeded ?
+                <form className="form" onSubmit={handleSubmit}>
+                    <div className="inputContainer">
+                        <label>Full Name</label>
+                        <input
+                            type="text"
+                            name="fullName"
+                            placeholder="Enter Your Name"
+                            required />
+                    </div>
+                    <div className="inputContainer">
+                        <label>Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Enter Your Email"
+                            required />
+                    </div>
+                    <div className="inputContainer">
+                        <label>Message</label>
+                        <textarea
+                            name="message"
+                            placeholder="Enter Your Message"
+                            required />
+                    </div>
+                    <div className="formBtn">
+                        <button className="btn-p">Send</button>
+                    </div>
+                </form> :
+                <div className="form">
+                    <span>Thanks for reaching out!</span>
+                    </div>
+            }
         </div>
     );
 }
