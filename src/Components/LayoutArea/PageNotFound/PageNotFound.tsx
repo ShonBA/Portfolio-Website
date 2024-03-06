8976c3931a7b521774deef121835c00139d70e5a
Shon Benayoun <shonba98@gmail.com>
2024-03-06 16:24:53 +0200

    add page not found

--- src/Components/LayoutArea/PageNotFound/PageNotFound.tsx
+++ src/Components/LayoutArea/PageNotFound/PageNotFound.tsx
@@ -1,9 +1,21 @@
+import { HashLink } from "react-router-hash-link";
 import "./PageNotFound.scss";
+import useTitle from "../../../Utils/UseTitle";
 
 function PageNotFound(): JSX.Element {
+    useTitle("Page Not Found | 404")
     return (
         <div className="PageNotFound">
-			Page Not Found - 404
+            <div className="notFoundContainer">
+                <header className="header">
+                    <h1 className="headerLine">Page Not Found - 404</h1>
+                </header>
+                <div className="content">
+                    <h4>Sorry, the page you are looking for does not exist.</h4>
+                    <h4>Click the button below to go back home</h4>
+                </div>
+                <HashLink smooth to={"/#top"} className="btn-p">Home</HashLink>
+            </div>
         </div>
     );
 }
--- src/Components/NavArea/Menu/Menu.tsx
+++ src/Components/NavArea/Menu/Menu.tsx
@@ -4,10 +4,10 @@ import "./Menu.scss";
 function Menu(): JSX.Element {
     return (
         <div className="Menu">
-            <HashLink smooth to={"#top"}>Home</HashLink>
-            <HashLink smooth to={"#aboutMainSection"}>About</HashLink>
-            <HashLink smooth to={"#projectsMainSection"}>Projects</HashLink>
-            <HashLink smooth to={"#contactMainSection"}>Contact</HashLink>
+            <HashLink smooth to={"/#top"}>Home</HashLink>
+            <HashLink smooth to={"/#aboutMainSection"}>About</HashLink>
+            <HashLink smooth to={"/#projectsMainSection"}>Projects</HashLink>
+            <HashLink smooth to={"/#contactMainSection"}>Contact</HashLink>
         </div>
     );
 }
