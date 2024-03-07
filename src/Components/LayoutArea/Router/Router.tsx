53b9b31b88ac76600b5e8568e0cb8fbd2335708d
Shon Benayoun <shonba98@gmail.com>
2024-03-07 17:29:53 +0200

    11

--- src/Components/LayoutArea/Router/Router.tsx
+++ src/Components/LayoutArea/Router/Router.tsx
@@ -2,15 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../../HomeArea/Home/Home";
 import PageNotFound from "../PageNotFound/PageNotFound";
 import "./Router.scss";
-import appConfig from "../../../Utils/AppConfig";
 
 function Router(): JSX.Element {
     return (
         <div className="Router">
             <Routes>
                 {/* Default Routes */}
-                <Route path={appConfig.baseRoute} element={<Home />} />
-                <Route path={appConfig.baseRoute + "*"} element={<PageNotFound />} />
+                <Route path="/Portfolio-Website/" element={<Home />} />
+                <Route path="*" element={<PageNotFound />} />
             </Routes>
         </div>
     );
--- src/Utils/AppConfig.ts
+++ src/Utils/AppConfig.ts
@@ -8,7 +8,10 @@ class AppConfig {
     public readonly gitHubUrl = "https://github.com/ShonBA";
     public readonly CvUrl = "https://drive.google.com/file/d/1RWPiDFizusmxvm7KZPK986HwlgciwAYK/view?usp=sharing";
     // Routes
-    public readonly baseRoute: string = "/Portfolio-Website/";
+    public readonly HomeRoute: string = "/home";
+    public readonly AboutRoute: string = "/about";
+    public readonly ProjectsRoute: string = "/projects";
+    public readonly ContactRoute: string = "/contact";
 }
 
 const appConfig = new AppConfig();
