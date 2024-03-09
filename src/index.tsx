7c104bf7ca2bddc1123338193656db5803801cf7
Shon Benayoun <shonba98@gmail.com>
2024-03-09 18:27:47 +0200

    ?

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import Layout from './Components/LayoutArea/Layout/Layout';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
@@ -8,9 +8,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <Layout />
-  </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
